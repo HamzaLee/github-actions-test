e8ba775e1886404095854cda16ce49af9b969e86
Hamza Lee <hamza.litayem@yahoo.com>
2019-10-11 11:33:48 +0100

    Update Program.cs

--- GithubActionsTest/GithubActionsTest/Program.cs
+++ GithubActionsTest/GithubActionsTest/Program.cs
@@ -6,7 +6,7 @@ namespace GithubActionsTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World?");
         }
     }
 }
